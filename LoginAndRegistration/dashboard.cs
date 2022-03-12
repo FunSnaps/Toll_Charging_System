1fd1ed8a1d79a2bf4cb3e72d6ede1a53f0484e03
MightyDuck-Gi <72032298+MightyDuck-Gi@users.noreply.github.com>
2022-03-12 21:19:48 +0000

    Add project files.

---
+++ LoginAndRegistration/dashboard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginAndRegistration
+{
+    public partial class dashboard : Form
+    {
+        //when the window has been created it will store user name and send as arrgument
+        public dashboard(string role)
+        {
+            InitializeComponent();
+            label1.Text = role;
+            //if its driver it will display the driver button 
+            if (role == "driver")
+            {
+                button1.Show();
+                button2.Hide();
+            }
+            //if its operator it will display the operator button 
+            else if (role == "operator")
+            {
+                button2.Show();
+                button1.Hide();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //pressing this button will take the user to driver payments ui
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //pressing this button will take the user to operator ui
+        }
+    }
+}
---
+++ LoginAndRegistration/frmLogin.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace LoginAndRegistration
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=db_user.mdb");
+        OleDbCommand cmd = new OleDbCommand();
+        OleDbDataAdapter da = new OleDbDataAdapter();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+            txtUsername.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string loginQuery = "SELECT * FROM tbl_users WHERE username= '" + txtUsername.Text + "' and password= '" + txtPassword.Text + "'";
+            cmd = new OleDbCommand(loginQuery, con);
+            OleDbDataReader dr = cmd.ExecuteReader();
+            if (dr.Read() == true)
+            {
+                //After checking user login will pass the use obj and get their roles
+                try
+                {
+                    object[] meta = new object[10];
+                    dr.GetValues(meta);
+                    new dashboard(dr.GetString(2)).Show();
+                    this.Hide();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password, Please Try Again", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUsername.Text = "";
+                txtPassword.Text = "";
+                txtUsername.Focus();
+            }
+            con.Close();
+        }
+
+        private void CheckboxShowPas_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CheckboxShowPas.Checked)
+            {
+                txtPassword.PasswordChar = '\0';         
+            }
+            else
+            {
+                txtPassword.PasswordChar = '•';
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            new frmRegister().Show();
+            this.Hide();
+        }
+    }
+}
